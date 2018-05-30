7579d48618e15354ee446137d7f2fad608ef976d
Alemas <mateusreckziegel@gmail.com>
2018-05-30 15:44:38 -0300

    Task 8 done

--- src/i_introduction/_8_Smart_Casts/n08SmartCasts.kt
+++ src/i_introduction/_8_Smart_Casts/n08SmartCasts.kt
@@ -5,14 +5,16 @@ import util.doc8
 
 // 'sealed' modifier restricts the type hierarchy:
 // all the subclasses must be declared in the same file
+
 sealed class Expr
 class Num(val value: Int) : Expr()
 class Sum(val left: Expr, val right: Expr) : Expr()
 
 fun eval(e: Expr): Int =
         when (e) {
-            is Num -> todoTask8(e)
-            is Sum -> todoTask8(e)
+            is Num -> e.value
+            is Sum -> eval(e.left) + eval(e.right)
+            else -> throw IllegalArgumentException("Argument not valid")
         }
 
 fun todoTask8(expr: Expr): Nothing = TODO(
